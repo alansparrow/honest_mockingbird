155c672ed7abdade9a7ecd50a28efcd023c680fd
baotrungtn <baotrungtn@gmail.com>
2017-09-27 06:10:55 +0900

    Add VOTE api

---
+++ news/shared_info.py
@@ -0,0 +1,5 @@
+class SharedInfo:
+    HOLD_VOTE_TYPE = 'hold'
+    BUY_VOTE_TYPE = 'buy'
+    SELL_VOTE_TYPE = 'sell'
+    NEUTRAL_VOTE_TYPE = 'neutral' # not vote
--- news/views.py
+++ news/views.py
@@ -14,94 +14,155 @@ from django.core.serializers import serialize
 from django.utils.encoding import force_text
 from django.core.serializers.json import DjangoJSONEncoder
 from django.views.decorators.csrf import csrf_exempt
+from .shared_info import SharedInfo
 
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the news index.")
 
+@csrf_exempt
 def create_user(request):
     return_data = {}
-    new_user = User(token = User.create_token())
-    new_user.save()
-    data['new_user'] = model_to_dict(new_user)
+
+    if (request.method == 'POST'):
+        new_user = User(token = User.create_token())
+        new_user.save()
+        return_data['new_user'] = model_to_dict(new_user)
 
     return JsonResponse(return_data, safe=False)
 
 @csrf_exempt
 def vote_hold(request):
-    user_token = request.POST['user_token']
-    news_id = request.POST['news_id']
-    vote_type = request.POST['vote_type']
-
     return_data = {}
 
-    user = User.get(token=user_token)
-    news = News.get(news_id=news_id)
+    if (request.method == 'POST'):
+        user_token = request.POST['user_token']
+        news_id = request.POST['news_id']
 
-    if (user == None or news == None):
-        return_data['result_code'] = 1
-        return_data['message'] = 'user or news not found'
-        return_data['user'] = user
-        return_date['news'] = news
-    else:
-        return_data['result_code'] = 0
-        return_data['message'] = 'ok'
-        vote = Vote.get(user_token=user_token, news_id=news_id)
+        user = User.get(token=user_token)
+        news = News.get(news_id=news_id)
 
-        if (vote != None):
-            if (vote.vote_type != 'hold'):                
-                if (vote.vote_type == 'buy'):
-                    News.vote_buy(news.id, -1)              
-                elif (vote.vote_type == 'sell'):
-                    News.vote_sell(news.id, -1)
-                News.vote_hold(news.id, 1)
-                vote.vote_type = vote_type
-                vote.save()
+        if (user == None or news == None):
+            return_data['result_code'] = 1
+            return_data['message'] = 'user or news not found'
+            return_data['user'] = None
+            return_data['news'] = None
         else:
-            News.vote_hold(news.id, 1)
-            vote = Vote(user_token=user_token, news_id=news_id, vote_type=vote_type)
-            vote.save()
+            return_data['result_code'] = 0
+            return_data['message'] = 'ok'
+            vote = Vote.get(user_token=user_token, news_id=news_id)
+
+            if (vote != None):
+                if (vote.vote_type != SharedInfo.HOLD_VOTE_TYPE):
+                    if (vote.vote_type == SharedInfo.BUY_VOTE_TYPE):
+                        News.vote_buy(news.id, -1)
+                    elif (vote.vote_type == SharedInfo.SELL_VOTE_TYPE):
+                        News.vote_sell(news.id, -1)
+                    News.vote_hold(news.id, 1)
+                    vote.vote_type = SharedInfo.HOLD_VOTE_TYPE
+                    vote.save()
+            else:
+                News.vote_hold(news.id, 1)
+                vote = Vote(user_token=user_token, news_id=news_id, vote_type=SharedInfo.HOLD_VOTE_TYPE)
+                vote.save()
 
     return JsonResponse(return_data, safe=False)
 
 
 @csrf_exempt
 def vote_buy(request):
-    user_token = request.POST['user_token']
-    news_id = request.POST['news_id']
-    vote_type = request.POST['vote_type']
+    return_data = {}
 
-    print(str(user_token) + '   ' + str(news_id) + '   ' + str(vote_type))
-    pass
+    if (request.method == 'POST'):
+        user_token = request.POST['user_token']
+        news_id = request.POST['news_id']
+
+        user = User.get(token=user_token)
+        news = News.get(news_id=news_id)
+
+        if (user == None or news == None):
+            return_data['result_code'] = 1
+            return_data['message'] = 'user or news not found'
+            return_data['user'] = None
+            return_data['news'] = None
+        else:
+            return_data['result_code'] = 0
+            return_data['message'] = 'ok'
+            vote = Vote.get(user_token=user_token, news_id=news_id)
+
+            if (vote != None):
+                if (vote.vote_type != SharedInfo.BUY_VOTE_TYPE):
+                    if (vote.vote_type == SharedInfo.HOLD_VOTE_TYPE):
+                        News.vote_hold(news.id, -1)
+                    elif (vote.vote_type == SharedInfo.SELL_VOTE_TYPE):
+                        News.vote_sell(news.id, -1)
+                    News.vote_buy(news.id, 1)
+                    vote.vote_type = SharedInfo.BUY_VOTE_TYPE
+                    vote.save()
+            else:
+                News.vote_buy(news.id, 1)
+                vote = Vote(user_token=user_token, news_id=news_id, vote_type=SharedInfo.BUY_VOTE_TYPE)
+                vote.save()
+
+    return JsonResponse(return_data, safe=False)
 
 @csrf_exempt
 def vote_sell(request):
-    user_token = request.POST['user_token']
-    news_id = request.POST['news_id']
-    vote_type = request.POST['vote_type']
+    return_data = {}
 
-    print(str(user_token) + '   ' + str(news_id) + '   ' + str(vote_type))
-    pass
+    if (request.method == 'POST'):
+        user_token = request.POST['user_token']
+        news_id = request.POST['news_id']
+
+        user = User.get(token=user_token)
+        news = News.get(news_id=news_id)
+
+        if (user == None or news == None):
+            return_data['result_code'] = 1
+            return_data['message'] = 'user or news not found'
+            return_data['user'] = None
+            return_data['news'] = None
+        else:
+            return_data['result_code'] = 0
+            return_data['message'] = 'ok'
+            vote = Vote.get(user_token=user_token, news_id=news_id)
+
+            if (vote != None):
+                if (vote.vote_type != SharedInfo.SELL_VOTE_TYPE):
+                    if (vote.vote_type == SharedInfo.HOLD_VOTE_TYPE):
+                        News.vote_hold(news.id, -1)
+                    elif (vote.vote_type == SharedInfo.BUY_VOTE_TYPE):
+                        News.vote_buy(news.id, -1)
+                    News.vote_sell(news.id, 1)
+                    vote.vote_type = SharedInfo.SELL_VOTE_TYPE
+                    vote.save()
+            else:
+                News.vote_sell(news.id, 1)
+                vote = Vote(user_token=user_token, news_id=news_id, vote_type=SharedInfo.SELL_VOTE_TYPE)
+                vote.save()
+
+    return JsonResponse(return_data, safe=False)
     
 def get_news(request):
-    before_pub_date = request.GET['pub_date'][:19]
-    news_count = request.GET['news_count']
-    if (news_count.isdigit()):
-        news_count = int(news_count)
-    else:
-        news_count = 20
-
-    before_pub_date = datetime.strptime(before_pub_date, "%Y-%m-%d %H:%M:%S")
-
-    news_list = News.objects.filter(pub_date__lt=before_pub_date).order_by('-pub_date')[:news_count]
-    items = []
-    for news in news_list:
-        items.append(model_to_dict(news))
-        
     return_data = {}
-    return_data['items'] = items
 
-    print(return_data)
+    if (request.method == 'GET'):
+        before_pub_date = request.GET['pub_date'][:19]
+        news_count = request.GET['news_count']
+        if (news_count.isdigit()):
+            news_count = int(news_count)
+        else:
+            news_count = 20
+
+        before_pub_date = datetime.strptime(before_pub_date, "%Y-%m-%d %H:%M:%S")
+
+        news_list = News.objects.filter(pub_date__lt=before_pub_date).order_by('-pub_date')[:news_count]
+        items = []
+        for news in news_list:
+            items.append(model_to_dict(news))
+
+
+        return_data['items'] = items
 
     return JsonResponse(return_data, safe=False)
